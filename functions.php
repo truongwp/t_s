ead644fddf2dc0459bfbdaa4b05c2a8a4a0b0602
Truong Giang <truongwp@gmail.com>
2017-03-31 15:37:24 +0700

    Doing sidebar layout feature

--- functions.php
+++ functions.php
@@ -141,3 +141,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Load sidebar layout feature.
+ */
+require get_template_directory() . '/inc/sidebar-layout/sidebar-layout.php';
---
+++ inc/sidebar-layout/sidebar-layout.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Sidebar layout feature
+ *
+ * @package T_S
+ */
+
+/**
+ * Class T_S_Sidebar_Layout
+ */
+class T_S_Sidebar_Layout {
+
+	/**
+	 * Class T_S_Sidebar_Layout construct.
+	 */
+	public function __construct() {
+		// Register customizer settings.
+		// Register meta box settings.
+		// Register term meta settings.
+	}
+
+	public static function get_value() {
+
+	}
+}
+new T_S_Sidebar_Layout();
